a50efcd1b03ac00d337ca81cf5e5eacac42fe221
sawadyecma <snowslidez@yahoo.co.jp>
2021-12-22 20:35:00 +0900

    add router

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,25 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
 const app = new Application();
 
-app.use((ctx) => {
-    ctx.response.body = "Hello World!";
+const router = new Router();
+router.get('/users', (ctx) => {
+    ctx.response.body = 'Received a GET HTTP method';
 });
 
+router.post('/users', (ctx) => {
+    ctx.response.body = 'Received a POST HTTP method';
+});
+
+router.put('/users/:userId', (ctx) => {
+    ctx.response.body = `PUT HTTP method on user ${ctx.params.userId}`;
+});
+
+router.delete('/users/:userId', (ctx) => {
+    ctx.response.body = `DELETE HTTP method on user ${ctx.params.userId}`;
+});
+
+app.use(router.routes());
 
 await app.listen({ port: 8000 });
+
