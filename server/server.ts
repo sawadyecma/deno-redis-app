28456a9e6588c978c4565429c76ca3a9db989fdf
sawadyecma <snowslidez@yahoo.co.jp>
2021-12-22 20:19:25 +0900

    fix server

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,19 @@
-import { serve } from "https://deno.land/std@0.118.0/http/server.ts";
+import { Status } from "https://deno.land/std@0.118.0/http/http_status.ts";
 
 console.log("http://localhost:8000/");
-serve((req) => new Response("Hello World\n"), { port: 8000 });
+const server = Deno.listen({port:8000})
+
+for await (const conn of server) {
+    // ...handle the connection...
+    (async () => {
+        handle(conn)
+    })();
+}
+
+
+async function handle(conn: Deno.Conn) {
+    const httpConn = Deno.serveHttp(conn);
+    for await (const requestEvent of httpConn) {
+        await requestEvent.respondWith(new Response("hello\n", {status: Status.OK}))
+    }
+}
