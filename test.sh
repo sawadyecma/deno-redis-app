6a20b0b796635a5699d09628b20b106ac10549d2
sawadyecma <snowslidez@yahoo.co.jp>
2021-12-24 09:40:02 +0900

    fix test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,9 @@
+echo
 curl -X GET http://localhost:8000/users/
-curl -X POST http://localhost:8000/users/11
+echo
+curl -X POST http://localhost:8000/users
+echo
 curl -X PUT http://localhost:8000/users/11
+echo
 curl -X DELETE http://localhost:8000/users/11
+echo
